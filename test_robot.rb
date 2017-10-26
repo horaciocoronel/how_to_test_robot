29e4727f115264cf1f260dd9adfb73a275f30f6e
horaciocoronel <arielc@gmail.com>
2017-10-26 14:41:52 -0400

    finish tests 5-8

--- test_robot.rb
+++ test_robot.rb
@@ -48,39 +48,48 @@ class TestRobot < MiniTest::Test
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
+    # skip
     # arrange
-
+    my_robot = Robot.new
     # act
-
+    my_test = my_robot.prioritize_tasks
     # assert
+    assert_equal( -1, my_test)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
+    # skip
     # arrange
-
+    my_robot = Robot.new
+    my_robot.todos = [3, 6, 1]
     # act
-
+    my_test = my_robot.prioritize_tasks
     # assert
+    assert_equal( 6, my_test)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
+    # skip
     # arrange
-
+    my_robot = Robot.new
+    day_off = true
     # act
-
+    my_test = my_robot.workday?(day_off)
     # assert
+    expected = true
+    assert_equal(expected, my_test)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
+    # skip
     # arrange
-
+    my_robot = Robot.new
+    day_off = false
     # act
-
+    my_test = my_robot.workday?(day_off)
     # assert
+    expected = true
+    assert_equal(expected, my_test)
   end
 
 end
